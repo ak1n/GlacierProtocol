2c3709d547ee7b3b1a97aa598d9715f047fc9f06
ak1n <11824083+ak1n@users.noreply.github.com>
2018-12-03 13:49:37 -0500

    minor updates, fixing comments

--- glacierscript.py
+++ glacierscript.py
@@ -424,24 +424,12 @@ def sign_transaction(source_address, keys, redeem_script, unsigned_hex, input_tx
     input_txs: List<dict> A list of input transactions to use (bitcoind decoded format)
 
     ak1n comments:
-      why is amount needed?
-        not used in Andresen/bitcoin.org examples
-          both use hex, tx data (txid, vout, scriptPubKey, redeemScript), signing key
-          bitcoin.org example: https://bitcoin.org/en/developer-examples#p2sh-multisig
-        amount should have been defined in the create transaction function & call (in destinations variable)
-        thus should be contained in the unsigned hex
-        amount commented out
-      IF REMOVE AMOUNT script crashes w:
-      bitcoin-cli createrawtransaction '[{"vout": 0, "txid": "c48ceaa706935e44e26cc2bbac182a8f6f4c9ed7eafe890cd6b3d8d0bb88e9f5"}]' '{"3H7VN9ikV7FoEHifeY6wArdJ5ew97k7Reu": 0, "bc1qmhmer7p9pvm3m50nw48alw0f552ng8kguagjgn": 0}'
-
-cli output:
-0200000001f5e988bbd0d8b3d60c89feead79e4c6f8f2a18acbbc26ce2445e9306a7ea8cc40000000000ffffffff02000000000000000017a914a92ac6e0099c2a54bf096a4df7e2309af85824df870000000000000000160014ddf791f8250b371dd1f3754fdfb9e9a515341ec800000000
-
-
-error code: -3
-error message:
-Missing amount for CTxOut(nValue=21000000.00000000, scriptPubKey=a914a92ac6e0099c2a54bf096a4df7)
-
+      - the usage of amount here seems complicated...normally would seemingly be inputted in createtransaction then that signed without specifically specifying amount
+        - e.g. bitcoin.org & Andresen examples
+        - both signs use hex, tx data (txid, vout, scriptPubKey, redeemScript), signing key
+        - amount normally defined in the create transaction function & call (in destinations variable) -> then contained in hex which is signed
+        - see https://bitcoin.org/en/developer-examples#p2sh-multisig
+      - however, it seems due to interactive input & error checking this is inputted in the sign call...without it an "amount missing" error is elicited
     """
 
     # For each UTXO used as input, we need the txid, vout index, scriptPubKey, amount, and redeemScript
